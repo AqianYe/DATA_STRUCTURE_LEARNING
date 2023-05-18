ae900212e09e44cf501787156c0bff09fb9e615f
AqianYe <2270121117@qq.com>
2023-05-18 16:57:37 +0800

    新增知识点2

--- DS_2.3/DS_2.3/test.c
+++ DS_2.3/DS_2.3/test.c
@@ -391,6 +391,8 @@
 //	return 0;
 //}
 
+
+
 //双链表
 //typedef struct DNode//定义双链表结点类型
 //{
@@ -463,4 +465,285 @@
 //		q->next->prior = p;
 //	free(q);//释放结点空间
 //	return true;
+//}
+
+//void DestroyList(DLinkList& L)
+//{
+//	//循环释放各个数据节点
+//	while (L->next != NULL)
+//		DeleteNextDNode(L);
+//	free(L);//释放头结点
+//	L = NULL;//头指针指向NULL
+//}
+
+//双链表的遍历
+//后向遍历
+//while (p != NULL)
+//{
+//	//对结点p做相应处理，如打印
+//	p = p->next;
+//}
+////前向遍历
+//while(p!=NULL)
+//{
+//	//对结点p做相应处理
+//	p = p->prior;
+//}
+////前向遍历（跳过头结点）
+//while (p->prior != NULL)
+//{
+//	//对结点p做相应处理
+//	p = p->prior;
+//}
+//双链表不可随机存取，按位查找、按值查找操作都只能用
+//遍历的方式实现，时间复杂度为O(n)
+
+
+
+//循环链表
+//循环单链表
+//typedef struct LNode//定义单链表结点类型
+//{
+//	ElemType data;//每个结点存放一个数据元素
+//	struct LNode* next;//指针指向下一个结点
+//}LNode，* LinkList;
+//
+//初始化一个循环单链表
+//bool InitList(LinkList& L)
+//{
+//	L = (LNode*)malloc(sizeof(LNode));//分配一个头结点
+//	if (L == NULL)//内存不足，分配失败
+//		return false;
+//	L->next = L;//头结点next指向头结点
+//	return true;
+//}
+//
+////判断循环单链表是否为空
+//bool Empty(LinkList L)
+//{
+//	if (L->next == L)
+//		return true;
+//	else
+//		return false;
+//}
+//
+////判断结点p是否为循环单链表的表尾结点
+//bool IsTail(LinkList L, LNode* p)
+//{
+//	if (p->next == L)
+//		return true;
+//	else
+//		return false;
+//}
+
+//循环双链表
+//typedef struct DNode
+//{
+//	ElemTyoe data;
+//	struct DNode* prior, * next;
+//}DNode, * DLinkList;
+//
+////初始化空的循环双链表
+//bool InitDLinkList(DLinkList& L)
+//{
+//	L = (DNode*)malloc(sizeof(DNode));//分配一个头结点
+//	if (L == NULL)
+//		return false;
+//	L->prior = L;//头结点的prior指向头结点
+//	L->next = L;//头结点的next指向头结点
+//	return true;
+//}
+//
+//判断循环双链表是否为空
+//bool Empty(DLinkList L)
+//{
+//	if (L->next == L)
+//		return true;
+//	else
+//		return false;
+//}
+//
+//void TestDLinkList()
+//{
+//	//初始化循环双链表
+//	DLinkList L;
+//	InitDLinkList(L);
+////...后续代码...
+//}
+//
+//判断结点p是否为循环双链表的表尾结点
+//bool IsTail(DLinkList L, DNode* p)
+//{
+//	if (p->next == L)
+//		return true;
+//	else
+//		return false;
+//}
+//
+//循环双链表的插入
+//在p结点之后插入s结点
+//bool InsertNextDNode(DNode* p, DNode* s)
+//{
+//	s->next = p->next;//将结点*s插入到结点*p之后
+//	p->next->prior = s;
+//	s->prior = p;
+//	p->next = s;
+//    return true;
+//}
+
+//循环双链表的删除
+//删除p的后继结点q
+//bool DeleteNextDNode(DNode* p)
+//{
+//	DNode* q = p->next;//找到p的后继结点q
+//	p->next = q->next;
+//	q->next->prior = p;
+//	free(q);//释放结点空间
+//	return true;
+//}
+
+//静态链表
+//静态链表的结构类型描述
+#define MaxSize 50//静态链表的最大长度
+//静态链表结构类型的定义1
+//struct Node
+//{
+//	ElemType data;//存储数据元素
+//	int next;//下一个元素的数组下标
+//};
+//
+//void TestLinkList()
+//{
+//	struct Node a[MaxSize];
+//	//...后续代码
+//}
+
+//静态链表结构类型的定义2
+//typedef struct//静态链表结构类型的定义
+//{
+//	ElemType data;//存储数据元素
+//	int next;//下一个元素的数组下标
+//}SLinkList[MaxSize];
+//
+//void TestSLinkList()
+//{
+//	SLinkList a;
+//	//...后续代码
+//}
+
+//初始化静态链表：把a[0]的next设为-1
+
+//查找：从头结点出发挨个往后遍历结点
+
+/*插入位序为i的结点：
+  1.找到一个空的结点，存入数据元素
+    如何判断结点是否为空？
+    可让next为某个特殊值，如-2
+  2.从头结点出发找到位序为i-1的结点
+  3.修改新结点的next*/
+
+/* 删除某个结点：
+   1.从头结点出发找到前驱节点
+   2.修改前驱节点的游标
+   3.被删除结点的next设为-2 */
+
+//大话数据结构示例
+//#define MaxSize 1000
+//typedef struct
+//{
+//	ElemType data;
+//	int cur;
+//}Component,SLinkList[MaxSize];
+//
+////将一维数组space中各分量链成一备用链表
+////space[0].cur为头指针，"0"表示空指针
+//bool InitList(SLinkList space)
+//{
+//	int i;
+//	for (i = 0; i < MaxSize - 1; i++)
+//		space[i].cur = i + 1;
+//	space[MaxSize - 1].cur = 0;
+//	//目前静态链表为空，最后一个元素的cur为0
+//	return true;
+//}
+//
+////静态链表的插入与删除
+//
+///*为了辨明数组中哪些分量未被使用，解决的办法是将所有未被
+//使用过的及已被删除的分量用游标链链成一个备用的链表，每当
+//进行插入时，便可以从备用链表上取得第一个结点作为待插入的
+//新结点*/
+//
+////若备用空间链表非空，则返回分配的结点下标，否则返回0
+//int Malloc_SLL(SLinkList space)
+//{
+//	int i = space[0].cur;
+//	/*当前数组的第一个元素的cur存的值,就是要返回的第一个
+//	备用的空闲的下标*/
+//	if (space[0].cur)
+//		space[0].cur = space[i].cur;
+//	/*由于要拿出一个分量来使用了，所以得把它的下一个分量
+//	用来做备用*/
+//	return i;
+//}
+//
+////在L中第i个元素之前插入新的数据元素e
+//bool ListInsert(SLinkList L, int i, ElemType e)
+//{
+//	int j, k, l;
+//	k = MaxSize - 1;//注意k首先是最后一个元素的下标
+//	if (i<1 || i>ListLength(L) + 1)
+//		return false;
+//	j = Malloc_SSL(L);//获得空闲分量的下标
+//	if (j)
+//	{
+//		L[j].data = e;//将数据赋值给此份量的data
+//		//找到第i个元素之前的位置
+//		for (l = 1; l < i - 1; l++)
+//			k = L[k].cur;
+//		//把第i个元素之前的cur赋值给新元素的cur
+//		L[j].cur = L[k].cur;
+//		//把新元素的下标赋值给第i个元素之前元素的cur
+//		L[k].cur = j;
+//		return true;
+//	}
+//	return false;
+//}
+//
+////将下标为k的空间结点回收到备用链表
+//void Free_SSL(SLinkList space, int k)
+//{
+//	//把第一个元素cur值赋给要删除的分量cur
+//	space[k].cur = space[0].cur;
+//	//把要删除的分量下标赋值给第一个元素的cur
+//	space[0].cur = k;
+//}
+//
+////删除在L中第i个数据元素e
+//bool ListDelete(SLinkList L, int i)
+//{
+//	int j, k;
+//	if (i<1 || i>ListLength(L))
+//		return false;
+//	k = MaxSize - 1;
+//	for (j = 1; j <= i - 1; j++)
+//		k = L[k].cur;
+//	j = L[k].cur;
+//	L[k].cur = L[j].cur;
+//	Free_SSL(L, j);
+//	return true;
+//}
+//
+///* 初始条件：静态链表L已存在
+//   操作结果：返回L中数据元素个数  */
+//int ListLength(SLinkList L)
+//{
+//	int j = 0;
+//	int i = L[MaxSize - 1].cur;
+//	while (i)
+//	{
+//		i = L[i].cur;
+//		j++;
+//	}
+//	return j;
 //}
